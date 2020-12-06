2481e80cbb3c04f71c07d63a8f3f0ef0ca6d996a
index01d <indevelope@gmail.com>
2020-12-06 19:15:53 +0500

    docker conf

--- pm2.config.js
+++ pm2.config.js
@@ -1,21 +1,19 @@
-/* eslint-disable */
-
 module.exports = {
   apps: [{
-    name: "from-scratch-app",
-    script: "./dist/server/server.js",
+    name: 'from-scratch-app',
+    script: './dist/server/server.js',
     env: {
-      NODE_ENV: "production",
+      NODE_ENV: 'production'
     }
   }],
   deploy: {
-    "production": {
-      "user": "user",
-      "host": ["78.47.148.217"],
-      "ref": "origin/ssr",
-      "repo": "git@github.com:indevelope/from_scratch_example.git",
-      "path": "/home/user/from-scratch-app",
-      "post-deploy": "yarn; yarn build:client; yarn build:server; pm2 startOrRestart pm2.config.js"
-    },
+    production: {
+      user: 'user',
+      host: ['78.47.148.217'],
+      ref: 'origin/ssr',
+      repo: 'git@github.com:indevelope/from_scratch_example.git',
+      path: '/home/user/from-scratch-app',
+      'post-deploy': 'yarn; yarn build:client; yarn build:server; pm2 startOrRestart pm2.config.js'
+    }
   }
 }
