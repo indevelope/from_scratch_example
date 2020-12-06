d9116b9da61874da3c9834dc3403f5d8fbb5887e
index01d <indevelope@gmail.com>
2020-12-06 17:18:09 +0500

    pm2

---
+++ pm2.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+module.exports = {
+  apps: [{
+    name: "from-scratch-app",
+    script: "./dist/server/server.js",
+    env: {
+      NODE_ENV: "production",
+    }
+  }],
+  deploy: {
+    "production": {
+      "user": "user",
+      "host": ["78.47.148.217"],
+      "ref": "origin/ssr",
+      "repo": "git@github.com:indevelope/from_scratch_example.git",
+      "path": "/home/user/from-scratch-app",
+      "post-deploy": "yarn; yarn build:client; yarn build:server; pm2 startOrRestart pm2.config.js"
+    },
+  }
+}
