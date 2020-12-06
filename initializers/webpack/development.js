e363f5ca06ef424cfcbf2447c01bb7f26b046afd
index01d <indevelope@gmail.com>
2020-12-06 13:12:22 +0500

    production config

---
+++ initializers/webpack/development.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: './src/index.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true
+  },
+  output: {
+    filename: '[name].js'
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  module: {
+    rules: [
+      { test: /\.js/, use: 'babel-loader' },
+      {
+        test: /\.css/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: '[local]--[hash:base64:5]',
+              }
+            }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      base: '/'
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  performance: { hints: false }
+};
---
+++ initializers/webpack/production.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    bundle: './src/index.js'
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(process.cwd(), 'dist/assets')
+  },
+  resolve: {
+    modules: [path.resolve(process.cwd(), 'src'), 'node_modules']
+  },
+  module: {
+    rules: [
+      { test: /\.js/, use: 'babel-loader' },
+      {
+        test: /\.css/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: '[local]--[hash:base64:5]',
+              }
+            }
+          },
+          'postcss-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      base: '/'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash].css'
+    })
+  ]
+};
